2c69e58ef9060cf2d9a71769e89cacc77e77ba4e
rjaduecorral <rjaduecorral@gmail.com>
2019-02-04 15:53:33 +0100

    copy+paste

---
+++ Esercizi.Net/Esercizi.Net.BusinesLogic/TransazionC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizi.Net.BusinesLogic
+{
+    public class TransazioneC : Transazione
+    {
+        public override decimal Importo { get; set; }
+    }
+
+}
